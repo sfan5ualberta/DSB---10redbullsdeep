1c7929293b0c94c26e44fcd54f8d772da7c7188a
= <=>
2023-01-07 16:20:25 -0700

    fixed card implementation

--- deck.py
+++ deck.py
@@ -1,13 +1,14 @@
+from card import Card
 class Deck():
     def __init__(self, name, private):
         self.name = name
-        self.card = []
+        self.cards = []
         self.private = private
     
     def addCard(self, question, answer):
-        card.append(Card(question, answer))
+        self.cards.append(Card(question, answer))
     
     def removeCard(self, cardNumber):
         if (len(self.card) <= cardNumber and cardNumber >= 0):
-            card.pop(cardNumber)
+            self.card.pop(cardNumber)
         
