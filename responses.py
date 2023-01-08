d08fd35308d416d7481070de7e26dc5f6a145ed9
= <=>
2023-01-07 17:00:06 -0700

    added more commands

--- responses.py
+++ responses.py
@@ -26,12 +26,17 @@ def get_response(message) -> str:
 
     if p_message == "help":
         help_message = ("These are the currently available commands: \n"
-                "\t !help: currently in use. \n"
+                "\t !help: currently in use, showing all commands and their functionalities. \n"
                 "\t !createDeck (Deck name): create a new deck to add cards to. \n"
                 "\t !deleteDeck (Deck name): delete an existing deck.\n"
                 "\t !browseDeck (Deck name): check all the cards in a deck \n"
+                "\t !decks: list all deck names \n"
                 "\t !addCard (Deck name) (Prompt) (Answer): add a card to a deck\n"
                 "\t !removeCard (Deck name) (Card #): remove a card in the deck \n"
+                "\t !review (Deck name): A random card is chosen in a deck, displaying its question.\n"
+                "\t !flip: Flip the card, showing the answer to the current prompt."
+                "\t !next: Show the next card. \n"
+                "\t !stop: stop reviewing. Return to normal interface. \n"
         )
         return help_message
     
