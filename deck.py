d2ab00e05bfd4df5281396fac8e5469ae55867e1
= <=>
2023-01-07 21:02:00 -0700

    fleshed out commands, missing mongo implementation

--- deck.py
+++ deck.py
@@ -1,16 +1,24 @@
 from card import Card
+import random
 class Deck():
-    def __init__(self, name, private):
+    def __init__(self, name):
         self.name = name
-        self.cards = []
-        self.private = private
-    
+        self.cards = []    
     def addCard(self, question, answer):
         self.cards.append(Card(question, answer))
     
     def removeCard(self, cardNumber):
-        if (len(self.card) <= cardNumber and cardNumber >= 0):
+        if (len(self.cards) <= cardNumber and cardNumber >= 0):
             self.cards.pop(cardNumber)
-    
+            
+    def showDeck(self):
+        for i in range(0, len(self.cards)):
+            print("%d : " % i, end='')
+            print(self.cards[i])
+
     def review(self):
-        pass
+        print(self.cards[0])
+        self.cards.append(self.cards.pop(self.curCard)) # move card to back
+
+    def shuffle(self):
+        random.shuffle(self.cards)
--- responses.py
+++ responses.py
@@ -8,60 +8,61 @@ def get_response(message, review) -> str:
         return "What's up?"
 
     if p_message[:11] == "createdeck ":
-        deckname = p_message[12:]
-        return deckname, p_message, "create"
+        deckname = p_message[11:]
+        # create empty deck object in mongodb with deckname
+        return "create"
 
     if p_message[:11] == "deletedeck ":
-        deckname = p_message[12:]
+        deckname = p_message[11:]
+        #delete deck from mongodb
         return "delete"
 
     if p_message[:11] == "browsedeck ":
-        deckname = p_message[12:]
+        deckname = p_message[11:]
+        # get deck object with deckname from mongo
+        Deck.showDeck()
         return "browse"
+    
+    if p_message == "decks":
+        #loop over all decks, printing their names
+        pass
 
     if p_message[:8] == "addcard ":
-        deckname = p_message[9:]
+        split_message = p_message.split()
+        if len(split_message) != 4:
+            return "invalid input"
+        deckname = split_message[1]
+        prompt = split_message[2]
+        answer = split_message[3]
+        # get deck object with deckname from mongo
+        Deck.addCard(prompt, answer)
         return "add"
 
     if p_message[:11] == "removecard ":
-        deckname = p_message[12:]
+        split_message = p_message.split()
+        if len(split_message) != 3:
+            return "invalid input"
+        deckname = split_message[1]
+        card_index = split_message[2]
+        Deck.removeCard(card_index)
+        
         return "remove"
-
-<<<<<<< HEAD
-    if p_message == "review":
-        return review
-    if p_message == "settrue":
-        review = True
-
-
-=======
-    if p_message[:5] == "review":
-        deckname = p_message[6:]
-        return "review"
     
-    if p_message[:3] == "flip":
-        return "review"
-    
-    if p_message[:3] == "next":
-        return "review"
+    if p_message[:7] == "review":
+        deckname = p_message[7:]
+        #get deck from mongo
+        Deck.review()
 
-    if p_message[:3] == "stop":
-        return "stop"
-    
->>>>>>> b676899358fe92f49e41a3ad4edc0fdb22119485
     if p_message == "help":
         help_message = ("These are the currently available commands: \n"
                 "\t !help: currently in use, showing all commands and their functionalities. \n"
                 "\t !createDeck (Deck name): create a new deck to add cards to. \n"
                 "\t !deleteDeck (Deck name): delete an existing deck.\n"
-                "\t !browseDeck (Deck name): check all the cards in a deck \n"
-                "\t !decks: list all deck names \n"
-                "\t !addCard (Deck name) (Prompt) (Answer): add a card to a deck\n"
-                "\t !removeCard (Deck name) (Card #): remove a card in the deck \n"
-                "\t !review (Deck name): A random card is chosen in a deck, displaying its question.\n"
-                "\t !flip: Flip the card, showing the answer to the current prompt."
-                "\t !next: Show the next card. \n"
-                "\t !stop: stop reviewing. Return to normal interface. \n"
+                "\t !browseDeck (Deck name): check all the cards in a deck. \n"
+                "\t !decks: list all deck names. \n"
+                "\t !addCard (Deck name) (Prompt) (Answer): add a card to a deck.\n"
+                "\t !removeCard (Deck name) (Card #): remove a card in the deck. \n"
+                "\t !review (Deck name): Display the next card in this deck.\n"
         )
         return help_message
     
